40c6cc2e5361a819c6e43953909742978b7c5001
James Morris <james.e.n.morris@gmail.com>
2015-06-24 20:07:28 +0100

    Header animation updated. Listing has title and pagination

--- site/src/js/main.js
+++ site/src/js/main.js
@@ -10,7 +10,7 @@ $(function(){
   $('h1').delay(150).animate({ opacity: 1 }, 1000)
   $('.intro-wrapper, .author-details').delay(1500).animate({ opacity: 1 }, 1200)
   $('.partner h3, .partner img').delay(2600).animate({ opacity: 1 }, 1000)
-  $('.stage-image').delay(3200).animate({ opacity: 1 }, 1000)
+  $('.stage-image').delay(3200).animate({ opacity: 1, marginTop: '10px' }, 1000)
 
 });	
 
